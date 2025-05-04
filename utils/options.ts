87f85957bc01ee68a614aa22c1285fe0f6f07763
Simran Singh <simran.develop@gmail.com>
2025-05-05 08:49:57 +1200

    feat: make paletteOptions generic to support different palette types

--- utils/options.ts
+++ utils/options.ts
@@ -1,7 +1,7 @@
 import { Option } from '@/types/common';
 import { Palette } from '@/types/enums';
 
-export const paletteOptions: Option[] = [
+export const paletteOptions: Option<Palette>[] = [
   { label: 'Tailwind', value: Palette.TAILWIND },
   { label: 'Material', value: Palette.MATERIAL },
   { label: 'Radix Dark', value: Palette.RADIX_DARK },
