7dfb2855d27e2c321af9aeba9655ae3d88042bc5
Simran Singh <simran.develop@gmail.com>
2025-05-09 21:37:19 +1200

    refactor: simplify FormatToggle component by removing props and using store for state management

--- components/FormatToggle.tsx
+++ components/FormatToggle.tsx
@@ -1,25 +1,20 @@
-import { Dispatch, SetStateAction } from 'react';
+const FormatToggle = () => {
+  const { activeFormat, setActiveFormat } = useStore();
 
-type FormatToggleProps = {
-  colorFormat: 'hex' | 'rgb';
-  setColorFormat: Dispatch<SetStateAction<'hex' | 'rgb'>>;
-};
-
-const FormatToggle = ({ colorFormat, setColorFormat }: FormatToggleProps) => {
   return (
     <div className="px-2">
-      <div className="flex h-5 w-full cursor-pointer rounded-[2px] bg-zinc-500 text-center font-mono leading-5 font-bold tracking-wider">
+      <div className="flex h-5 w-full cursor-pointer rounded-[2px] bg-slate-500 text-center font-mono leading-5 font-bold tracking-wider">
         <span
           role="button"
-          className={`flex-1 rounded-l-[2px] pt-[0.5px] ${colorFormat === 'hex' ? 'text-lime-300' : 'bg-zinc-600 text-zinc-400'}`}
-          onClick={() => setColorFormat('hex')}
+          className={`flex-1 rounded-l-[2px] pt-[0.5px] ${activeFormat === 'hex' ? 'text-lime-300' : 'bg-slate-600 text-slate-400'}`}
+          onClick={() => setActiveFormat('hex')}
         >
           HEX
         </span>
         <span
           role="button"
-          className={`flex-1 rounded-r-[2px] pt-[0.5px] ${colorFormat === 'rgb' ? 'text-lime-300' : 'bg-zinc-600 text-zinc-400'}`}
-          onClick={() => setColorFormat('rgb')}
+          className={`flex-1 rounded-r-[2px] pt-[0.5px] ${activeFormat === 'rgb' ? 'text-lime-300' : 'bg-slate-600 text-slate-400'}`}
+          onClick={() => setActiveFormat('rgb')}
         >
           RGB
         </span>
