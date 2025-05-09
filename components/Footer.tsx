b96cda7fd85acd1466d81542f6544e20c39d32a0
Simran Singh <simran.develop@gmail.com>
2025-05-09 21:37:09 +1200

    fix: enhance Footer component by adding click functionality to GitHub logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,13 @@ type FooterProps = {};
 const Footer = ({}: FooterProps) => {
   return (
     <div className="px-3 py-2">
-      <img src={GitHubLogo} className="h-6 cursor-pointer" alt="GitHub" title="GitHub" />
+      <img
+        src={GitHubLogo}
+        className="h-6 cursor-pointer"
+        alt="GitHub"
+        title="GitHub"
+        onClick={() => window.open('https://github.com/SimranDev/color-lift', '_blank')}
+      />
     </div>
   );
 };
