a939337ace664524b41b6ae0cb67af4894ca3b9f
Simran Singh <simran.develop@gmail.com>
2025-05-16 20:20:38 +1200

    feat: enhance footer layout and add ColorLift logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,24 @@
 import GitHubLogo from '@/assets/github.png';
+import WebLogo from '@/assets/web.png';
 
 type FooterProps = {};
 
 const Footer = ({}: FooterProps) => {
   return (
-    <div className="px-3 py-2">
+    <div className="flex items-center gap-2 px-3 py-2">
       <img
         src={GitHubLogo}
-        className="h-6 cursor-pointer"
+        className="h-[21px] cursor-pointer"
         alt="GitHub"
         title="GitHub"
         onClick={() => window.open('https://github.com/SimranDev/color-lift', '_blank')}
+      />
+      <img
+        src={WebLogo}
+        className="mt-[1px] h-6 cursor-pointer"
+        alt="ColorLift"
+        title="ColorLift"
+        onClick={() => window.open('https://lift.codesim.dev', '_blank')}
       />
     </div>
   );
