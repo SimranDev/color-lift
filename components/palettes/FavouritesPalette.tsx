b49a4998673dc54d2924532b65476cb8fbb3f8cb
Simran Singh <81152234+SimranDev@users.noreply.github.com>
2026-08-13 10:49:26 +1200

    fix: use React attribute names on the favourites remove icon
    fill-rule and clip-rule are ignored by React; the camelCase forms are
    what actually reach the DOM.

--- components/palettes/FavouritesPalette.tsx
+++ components/palettes/FavouritesPalette.tsx
@@ -36,9 +36,9 @@ const FavouritesPalette = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="7" height="7" viewBox="0 0 14 14">
                       <path
                         fill="#fff"
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M1.707.293A1 1 0 0 0 .293 1.707L5.586 7L.293 12.293a1 1 0 1 0 1.414 1.414L7 8.414l5.293 5.293a1 1 0 0 0 1.414-1.414L8.414 7l5.293-5.293A1 1 0 0 0 12.293.293L7 5.586z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       />
                     </svg>
                   </div>
